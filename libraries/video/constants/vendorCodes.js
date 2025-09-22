a4dfe6472e56152125b30c000a8c66823d45d482
GuillQuirin <gui.quirin@hotmail.fr>
2025-09-22 10:26:02 +0200

    add videomodule

--- libraries/video/constants/vendorCodes.js
+++ libraries/video/constants/vendorCodes.js
@@ -1,8 +1,8 @@
 // Video Vendors
-export const DIGITEKA_VENDOR = -1;
 export const JWPLAYER_VENDOR = 1;
 export const VIDEO_JS_VENDOR = 2;
 export const AD_PLAYER_PRO_VENDOR = 3;
+export const DIGITEKA_VENDOR = 9999;
 
 // Ad Server Vendors
 export const GAM_VENDOR = 'gam';
